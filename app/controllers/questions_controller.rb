f0b0ae051f9d73af45b48afa537eea7ef64ce700
Callum Tipper <callum.tipper@gmail.com>
2018-07-30 12:41:37 +0800

    working stupid coach with tests for each condition

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+
+  # ANSWERS = {
+  #   1 => "Great!",
+  #   2 => "Silly question, get dressed and go to work!",
+  #   3 => "I don't care, get dressed and go to work!"
+  # }
+  def ask
+
+  end
+
+  def answer
+    if params[:question] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:question].last == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
